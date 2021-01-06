539661c296bc8799266245bd5aaa52709d64f4c4
gxmodzz <jannik.seidl2005@gmail.com>
2021-01-06 16:36:13 +0100

    update

--- bot.js
+++ bot.js
@@ -1,12 +1,27 @@
 const dbd = require("dbd.js")
  
+// MAIN
+
 const bot = new dbd.Bot({
-token: "TOKEN", 
-prefix: "!" 
+token:process.env.token,        // GO TO YOUR ".env" FILE AND PUT YOUR TOKEN IN THERE
+prefix: "$getServerVar[prefix]"
 })
+
+
+// VARIABLES
+
+
+bot.variables({
+    prefix: "!"  // YOU CAN CHANGE YOUR DEFAULT PREFIX TO WHATEVER YOU WANT
+  });
  
 bot.onMessage()
  
+
+
+
+// COMMAND HANDLER
+
 const fs = require('fs')
 
 const folders = fs.readdirSync("./commands/")
